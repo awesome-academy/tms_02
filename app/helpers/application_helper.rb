8c3a1ce93c6fc382e3dc8f957142049336d8d2c7
BlackPearl96 <van9665@gmail.com>
2019-04-22 16:57:13 +0700

    login user supervisor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def full_title page_title
+    base_title = I18n.t("title")
+    if page_title.blank?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
+  def is_supervisor? user
+    current_user.supervisor? && user.id == current_user.id
+  end
 end
